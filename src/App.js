9c80df74c8074be667b8e1a787bf70789a2bb9d3
Akovi Mensah <akovi66@gmail.com>
2023-02-04 03:39:40 -0500

    added header and footer components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Homepage</h1>
+      <Header />
+      <Homepage />
+      <Footer />
     </div>
   );
 }
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homepage = () => {
+    return (
+        <main>
+            <h1>Homepage</h1>
+        </main>
+    )
+}
+
+export default Homepage
